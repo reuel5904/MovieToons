6a5b6c281f654f4aa546254f2d2ff90e72d54bf2
reuel5904 <reuelpalancas@gmail.com>
2021-12-07 21:30:38 -0500

    Fix requests

--- src/Requests.jsx
+++ src/Requests.jsx
@@ -3,7 +3,7 @@ const API_KEY = "52b74967f028be8c2e4c82fe1369a733"
 const requests = {
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
     fetchPopular: `/movie/popular?api_key=${API_KEY}&language=en-US`,
-    fetchSearch: `/search/movie/?api_key=${API_KEY}&language=en-US&query=`,
+    fetchSearch: `/search/movie?api_key=${API_KEY}&language=en-US&query=`,
     fetchUpcoming: `/movie/upcoming?api_key=${API_KEY}&language=en-US`,
     fetchLatest: `/movie/latest?api_key=${API_KEY}&language=en-US`,
     fetchNowPlaying: `/movie/now_playing?api_key=${API_KEY}&language=en-US`
